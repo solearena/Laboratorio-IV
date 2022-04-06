0f5b373fe2e05b051d91af74a50db6712b6d0d16
solearena <solearena@gmail.com>
2022-04-05 23:36:14 -0300

    Getters y Setters

--- TP2_GRUPO_2/src/ejercicio1/Profesor.java
+++ TP2_GRUPO_2/src/ejercicio1/Profesor.java
@@ -15,6 +15,22 @@ public class Profesor extends Empleado{
 		this.antiguedadDocente = antiguedadDocente;
 	}
 
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+
 	@Override
 	public String toString() {
 		return "Profesor/a: ID=" + this.getId() + ", nombre=" + this.getNombre() + ", edad=" + this.getEdad() + ", cargo=" + cargo + ", antiguedad docente=" + antiguedadDocente;
